57f934e1b9fef48b3a93cdf4ba765ae9a183b7ac
晔峰 <tjf141457@alibaba-inc.com>
2020-03-10 09:48:25 +0800

    minor

--- train_seed.sh
+++ train_seed.sh
@@ -4,7 +4,7 @@
 # parser.add_argument('--seed', type=int, default=913, help="random seed for initialization")
 # parser.add_argument('--log', type=str, default='log', help="log file")
 
-for i in {0:7}:
+for i in {0..7}
 do
     CUDA_VISIBLE_DEVICES=$i python train_generator.py --option train --model model/c4ad96d_seed$i --batch_size 384 --max_seq_length 50\
     --seed $i &
